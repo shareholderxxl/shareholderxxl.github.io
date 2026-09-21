01ed52d7ed38e113f97bf0d030f57bdbcde68233
Hermes <hermes@portablewhisper.de>
2026-09-21 18:00:47 +0200

    Deploy portabletranscribe 2026-09-21T16:00Z

--- config.js
+++ config.js
@@ -2,4 +2,7 @@ window.__CONFIG__ = {
   VITE_MODEL_SOURCE: 'remote',
   VITE_MODEL_REPO: 'efederici/parakeet-tdt-0.6b-v3-onnx-int4',
   VITE_MODEL_REVISION: 'main',
+  VITE_MODEL_DECODER_REPO: 'Olicorne/parakeet-tdt-0.6b-v3-optimized-onnx',
+  VITE_MODEL_DECODER_SUBFOLDER: 'int8',
+  VITE_MODEL_DECODER_FILE: 'decoder_joint-model.int8.onnx',
 };
